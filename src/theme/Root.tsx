76a265eb3c99fba4060dd3d502fef27cd4301d13
Jaap-Hein Wester <j.h.wester@setonix.nl>
2025-02-07 10:27:31 +0100

    WIP tweaking

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,16 +1,16 @@
 import type { PropsWithChildren } from 'react';
-import '@rijkshuisstijl-community/design-tokens/dist/index.css'
-import '../css/rhc-overrides.css'
+//import '@rijkshuisstijl-community/design-tokens/dist/index.css'
+import '../../proprietary/design-tokens/dist/theme.css';
 
 // Default implementation, that you can customize
 function Root({ children }: PropsWithChildren<object>) {
     return (
-<div className="rhc-theme">
+<div className="don-theme">
     <header className='ro-header'>
         <h1 className='ro-header__site-name'><img src='/img/logo.svg' className='ro-header__site-logo' alt='Logo Developer Overheid' />Developer Overheid</h1>
         <div className='ro-header__ro-logo'><img src='/img/logo-rijksoverheid.svg' alt='Logo Rijksoverheid' /></div>
     </header>
-    {children}
+    {children} 
 </div>
     );
 }
---
+++ src/theme/icons/IconDocumentBlanco.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from "react";
+/**
+ * Rijkshuisstijl icoon
+ *
+ * @param {SVGProps<SVGSVGElement>} props SVG Attributes
+ * @returns {*} SVG Element
+ *
+ * Copyright Rijksoverheid
+ * 
+ * Er gelden auteursrechten op de huisstijl en dit icoon.
+ * Alleen partijen die een opdracht uitvoeren voor de Rijksoverheid en 
+ * daarvoor gebruik mogen maken van de huisstijl en het logo hebben 
+ * toestemming. Maar in alle andere gevallen is ieder gebruik verboden. 
+ */
+const IconDocumentBlanco = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" id="Laag_1" viewBox="0 0 64 64" width="1em" height="1em" data-icon-name="document-blanco" {...props}><defs><clipPath id="clippath"><path d="M0 0h64v64H0z" className="cls-1" /></clipPath><clipPath id="clippath-1"><path d="M0 0h64v64H0z" className="cls-1" /></clipPath><style>{".cls-1{fill:none}"}</style></defs><g style={{
+    clipPath: "url(#clippath)"
+  }}><path d="M51.41 22.41 36.58 7.58c-.38-.38-.88-.59-1.41-.59H14a2 2 0 0 0-2 2V55c0 1.1.9 2 2 2h36c1.1 0 2-.9 2-2V23.83c0-.53-.21-1.04-.59-1.41m-1.27 1.94-3.15.93V52H17V12h16.71l.93-3.14c.11-.34.36-.23.36-.23V23c0 .55.45 1 1 1h14.37s.12.25-.22.36" style={{
+      clipPath: "url(#clippath-1)"
+    }} /></g></svg>;
+export default IconDocumentBlanco;
