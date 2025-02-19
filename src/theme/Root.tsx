bea96ad9ff2274a1032f1b1a33b6b16179202b0f
Tom Ootes <tom@ootes.io>
2025-02-19 10:48:25 +0100

    fix: remove log from theme Root.tsx

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -6,24 +6,22 @@ import { useLocation } from '@docusaurus/router';
 
 // Default implementation, that you can customize
 function Root({ children }: PropsWithChildren<object>) {
-  const {siteConfig} = useDocusaurusContext();
-  let {pathname} = useLocation();
+  const { siteConfig } = useDocusaurusContext();
+  let { pathname } = useLocation();
   pathname = pathname.replace('/', '');
 
-  //console.log(useDocusaurusContext(), useLocation());
-
   return (
-<div className="don-theme" data-page={pathname === '' ? 'home' : pathname}>
-    <header className='ro-header container'>
+    <div className="don-theme" data-page={pathname === '' ? 'home' : pathname}>
+      <header className='ro-header container'>
         {/* <h1 className='ro-header__site-name'><img src='/img/logo.svg' className='ro-header__site-logo' alt='Logo Developer Overheid' />Developer Overheid</h1> */}
         <div className='ro-header__word-mark'>
-            <h1 className='ro-header__site-name' title={`${siteConfig.title} | ${siteConfig.customFields.siteName}`}>{`${siteConfig.customFields.siteName}`}</h1>
-            <p className='ro-header__pay-off'>{siteConfig.tagline}</p>
+          <h1 className='ro-header__site-name' title={`${siteConfig.title} | ${siteConfig.customFields.siteName}`}>{`${siteConfig.customFields.siteName}`}</h1>
+          <p className='ro-header__pay-off'>{siteConfig.tagline}</p>
         </div>
         <div className='ro-header__ro-logo'><img src='/img/logo-rijksoverheid.svg' alt='Logo Rijksoverheid' /></div>
-    </header>
-    {children} 
-</div>
+      </header>
+      {children}
+    </div>
   );
 }
 
