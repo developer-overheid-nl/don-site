84ec14f3dc8c5da46b52d1ea7b80b44ef9bf62b6
Dimitri van Hees <dimitri@jumpventures.nl>
2025-05-07 13:28:12 +0200

    Update src/components/TeamProfileCards/index.tsx

--- src/components/TeamProfileCards/index.tsx
+++ src/components/TeamProfileCards/index.tsx
@@ -129,7 +129,7 @@ export function ActiveTeam(): ReactNode {
           en heeft een grote passie voor standaarden. Hij gelooft in de kracht
           van heldere afspraken en consistente structuren om samenwerking en
           innovatie te versnellen. In zijn vrije tijd is hij een fanatiek
-          mountainbiker en gaat hij graag kamperen in de natuur.
+          mountainbiker en gaat hij graag schaatsen.
         </Translate>
       </TeamProfileCardCol>
       <TeamProfileCardCol
