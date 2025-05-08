b0169eae1b4d46ca2b4332d7c866b7761a1229fe
Jaap-Hein Wester <j.h.wester@setonix.nl>
2025-05-08 15:45:27 +0200

    TEMP prod docusaurus url back again

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
   ],
 
   // Set the production url of your site here
-  url: "https://don.apps.digilab.network",
+  url: "https://developer.overheid.nl",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
