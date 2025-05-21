fc32f5d9b32f00272654d1a765f6b69511f5da89
pasibun <matthijs_hovestad@hotmail.com>
2025-05-21 12:16:18 +0200

    wijziging bewerk deze pagina voorstel

---
+++ src/theme/EditThisPage/index.tsx
@@ -0,0 +1,46 @@
+import React, { type ReactNode } from "react";
+import Link from "@docusaurus/Link";
+import IconEdit from "@theme/Icon/Edit";
+import { useLocation } from "@docusaurus/router";
+import type { Props } from "@theme/EditThisPage";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import IconLampBrand from "../icons/IconLampBrand";
+
+export default function EditThisPage({ editUrl }: Props): ReactNode {
+  const { pathname } = useLocation();
+  const isBlogPost = pathname.startsWith("/blog/");
+
+  return (
+    <div className={ThemeClassNames.common.editThisPage}>
+      {isBlogPost ? (
+        <>
+          <span>
+            <Link to="https://github.com/developer-overheid-nl/don-site/issues/new/choose">
+              <IconEdit />
+              Heb jij ook een idee voor een blog?
+            </Link>
+          </span>
+        </>
+      ) : (
+        <>
+          <div>
+            <span>
+              <Link to={editUrl}>
+                <IconEdit />
+                Iets van dit artikel verbeteren?
+              </Link>
+            </span>
+          </div>
+          <div>
+            <span>
+              <Link to="https://github.com/developer-overheid-nl/don-site/issues/new/choose">
+                <IconLampBrand width="25px" height="25px" />
+                Heb je nog andere ideeën of suggesties?
+              </Link>
+            </span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
