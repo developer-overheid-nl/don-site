a798af5e5dd478a4aaf1107848c000956e4adeb1
Jaap-Hein Wester <j.h.wester@setonix.nl>
2026-07-09 14:30:07 +0200

    Smoke test kennisbank on custom icons

--- cypress/e2e/kennisbank.cy.js
+++ cypress/e2e/kennisbank.cy.js
@@ -7,7 +7,12 @@ describe('Kennisbank', () => {
     cy.visit('/kennisbank');
 
     cy.get('main h1').should('have.text', 'Kennisbank');
-    cy.contains(/Er zijn op dit moment \d+ artikelen in onze Kennisbank./)
+    cy.contains(/Er zijn op dit moment \d+ artikelen in onze Kennisbank./);
+  });
 
+  it('has custom icons', () => {
+    cy.visit('/kennisbank');
+
+    cy.get('[data-icon-name="lade-archiefkast"]').should('exist');
   });
 });
