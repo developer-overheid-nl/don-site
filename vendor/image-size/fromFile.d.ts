3dd5bc9e2be94315e877c93946186778674bbbcb
pasibun <matthijs_hovestad@hotmail.com>
2026-08-18 12:13:11 +0200

    chore: rely on Docusaurus image-size [deploy-test]

--- vendor/image-size/fromFile.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ISize } from "./index";
-import type { Readable } from "node:stream";
-
-export declare function imageSizeFromFile(filePath: string): Promise<ISize>;
-export declare function imageSizeFromStream(stream: Readable): Promise<ISize>;
-export declare function setConcurrency(concurrency: number): void;
